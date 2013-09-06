a6582f0fed23d8d1a07560ea779db4f8c80d8f46
Prashant Jalan <prashant@skynet.(none)>
2013-09-07 02:06:32 +0530

    Revert "Proceeding forward"
    This reverts commit 1cfd8cbb68ca77cf0e4b33faa1f2124b4c248b85.

--- tcpclient.py
+++ tcpclient.py
@@ -6,11 +6,7 @@ import sys
 def main():
 	
 	#Constants		
-<<<<<<< HEAD
 	PORT = 5160
-=======
-	PORT = 5036
->>>>>>> parent of eda64c2... Working primitively
 	ADDRESS = "localhost"
 
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
