84c4c24de6bfb5277d11923ab4450a7198c6b804
Prashant Jalan <prashant@skynet.(none)>
2013-09-07 00:34:08 +0530

    Problem of shared cDict encountered. Need to figure a way out

--- tcpserver.py
+++ tcpserver.py
@@ -1,5 +1,5 @@
 import socket
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Value
 import os
 import sys
 import time
@@ -8,13 +8,15 @@ import time
 def main():
 	
 	#Constants
-	PORT = 5031
+	PORT = 5036
 	ADDRESS = "localhost"
-	MAX_CLIENT = 2
+	MAX_COUNT1 = 2				#Change these to schedule the processes
+	MAX_COUNT2 = 5
+	MAX_COUNT3 = 3
+	MAX_COUNT4 = 2
 	q1 = Queue()				#Buffer queue for register
 	q2 = Queue()				#Buffer queue for sharing
 	q3 = Queue()				#Buffer queue for search
-	qc = Queue()				#Buffer queue for count
 	qd = Queue()				#Buffer queue for closed clients
 	cDict = {}					#Dictionary for storing the clients
 
@@ -25,20 +27,33 @@ def main():
 
 	sys.stdout.write("Server running...\n");
 
+	#Starting the queue handling processes
+	for i in range(MAX_COUNT1):
+		qH = Process(target=q1Handler, args=(qd,q1,cDict))
+		qH.start()
+	for i in range(MAX_COUNT2):
+		qH = Process(target=q2Handler, args=(qd,q2,cDict))
+		qH.start()
+	for i in range(MAX_COUNT3):
+		qH = Process(target=q3Handler, args=(qd,q3,cDict))
+		qH.start()
+	
 	#Connecting to new Clients
+	#All the I/O operations are also done here
 	KEY = 0													#Unique key identifier for each client
-	DELETED = 0												#No. of clients processed
+	count4 = Value('i', 0)
 	while 1:
 		#Checking for clients
-		if KEY-DELETED<MAX_CLIENT:
+		if count4.value < MAX_COUNT4:
 			client_socket, address = server_socket.accept()
 			try:
 				PORT += 1
 				KEY += 1
 				cDict[str(KEY)]=[client_socket,address]
 				client_socket.send(str(PORT))				#Sending port to make a server
-				time.sleep(0.05)							#Time reqd. to send the above packet
-				clientHandler = Process(target=newClient, args=(client_socket,address,qd,q1,q2,q3,KEY))
+				time.sleep(0.5)								#Time reqd. to send the above packet
+				count4.value += 1
+				clientHandler = Process(target=newClient, args=(client_socket,address,count4,qd,q1,q2,q3,KEY))
 				clientHandler.start()
 			except:
 				continue
@@ -51,11 +66,48 @@ def main():
 		while not qd.empty():
 			temp = qd.get()
 			del cDict[temp]
-			DELETED += 1
-			
 
 
-def newClient(client_socket,address,qd,q1,q2,q3,KEY):
+
+def q1Handler(qd,q1,cDict):
+	#This process empties the queue, updates the data structures.
+	while 1:
+		KEY = q1.get()
+		print cDict
+		[client_socket, address] = cDict[KEY]
+		try:
+			client_socket.send("Let's begin the registration process!\n")
+		except:
+			sys.stdout.write(str(address)+" closed due to error.\n")
+			qd.put(KEY)
+
+
+
+def q2Handler(qd,q2,cDict):
+	#This process empties the queue, updates the data structures.
+	while 1:
+		KEY = q2.get()
+		[client_socket, address] = cDict[KEY]
+		try:
+			client_socket.send("Knowledge grows when shared!\n")
+		except:
+			sys.stdout.write(str(address)+" closed due to error.\n")
+			qd.put(KEY)
+
+
+def q3Handler(qd,q3,cDict):
+	#This process empties the queue, updates the data structures.
+	while 1:
+		KEY = q3.get()
+		[client_socket, address] = cDict[KEY]
+		try:
+			client_socket.send("Enter your search string:\n")
+		except:
+			sys.stdout.write(str(address)+" closed due to error.\n")
+			qd.put(KEY)		
+
+
+def newClient(client_socket,address,count4,qd,q1,q2,q3,KEY):
 	#This function will take input from the client and accordingly
 	#add it to its buffer queue
 	tmp0 = ""
@@ -88,10 +140,12 @@ def newClient(client_socket,address,qd,q1,q2,q3,KEY):
 				sys.stdout.write(str(address)+" closed.\n")
 				qd.put(str(KEY))
 				flag = False
+				count4.value = count4.value-1
 		except:
 			sys.stdout.write(str(address)+" closed due to error.\n")
 			qd.put(str(KEY))
 			flag = False
+			count4.value = count4.value-1
 		break
 
 	if flag:
