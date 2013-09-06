9045d1d0d68687c04c2b58a74ff983ae9f594cc0
Prashant Jalan <prashant@skynet.(none)>
2013-09-06 23:38:21 +0530

    Improved further. Added hash functions for storing client address.

--- tcpserver.py
+++ tcpserver.py
@@ -8,13 +8,15 @@ import time
 def main():
 	
 	#Constants
-	PORT = 5020
+	PORT = 5031
 	ADDRESS = "localhost"
 	MAX_CLIENT = 2
 	q1 = Queue()				#Buffer queue for register
 	q2 = Queue()				#Buffer queue for sharing
 	q3 = Queue()				#Buffer queue for search
 	qc = Queue()				#Buffer queue for count
+	qd = Queue()				#Buffer queue for closed clients
+	cDict = {}					#Dictionary for storing the clients
 
 	#Assigning server
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,24 +26,40 @@ def main():
 	sys.stdout.write("Server running...\n");
 
 	#Connecting to new Clients
+	KEY = 0													#Unique key identifier for each client
+	DELETED = 0												#No. of clients processed
 	while 1:
-		if qc.qsize()<MAX_CLIENT:
+		#Checking for clients
+		if KEY-DELETED<MAX_CLIENT:
 			client_socket, address = server_socket.accept()
-			PORT += 1
-			client_socket.send(str(PORT))				#Sending port to make a server
-			time.sleep(0.05)							#Time reqd. to send the above packet
-			qc.put('X')									#Maintaining the no. of processes
-			clientHandler = Process(target=newClient, args=(client_socket,address,qc,q1,q2,q3))
-			clientHandler.start()
+			try:
+				PORT += 1
+				KEY += 1
+				cDict[str(KEY)]=[client_socket,address]
+				client_socket.send(str(PORT))				#Sending port to make a server
+				time.sleep(0.05)							#Time reqd. to send the above packet
+				clientHandler = Process(target=newClient, args=(client_socket,address,qd,q1,q2,q3,KEY))
+				clientHandler.start()
+			except:
+				continue
 		else:
 			client_socket, address = server_socket.accept()
 			client_socket.send('Q')
 			client_socket.close()
+		
+		#Deleting closed clients
+		while not qd.empty():
+			temp = qd.get()
+			del cDict[temp]
+			DELETED += 1
+			
 
 
-
-def newClient(client_socket,address,qc,q1,q2,q3):
+def newClient(client_socket,address,qd,q1,q2,q3,KEY):
+	#This function will take input from the client and accordingly
+	#add it to its buffer queue
 	tmp0 = ""
+	flag = True
 	sys.stdout.write("Got a connection from "+str(address)+'\n')
 	i = 0
 	while 1:
@@ -54,26 +72,31 @@ def newClient(client_socket,address,qc,q1,q2,q3):
 			tmp5 = "3: Search for a file\n"
 			tmp6 = "Q: Quit\n"
 			client_socket.send(tmp0+tmp1+tmp2+tmp3+tmp4+tmp5+tmp6)
-			data = client_socket.recv(1)
+			data = client_socket.recv(8)
 			if data=='1':
-				q1.put('X')
+				q1.put(str(KEY))
 			elif data=='2':
-				q2.put('X')
+				q2.put(str(KEY))
 			elif data=='3':
-				q3.put('X')
-			elif data!='Q' and i<=3:
+				q3.put(str(KEY))
+			elif i<=3 and data!='Q':
 				tmp0 = "\nNot a valid input. Let's try again...\n"
 				continue
-			client_socket.send('Q')
-			client_socket.close()
-			sys.stdout.write(str(address)+" closed.\n")
-			qc.get()
-			break
+			else:
+				client_socket.send('Q')
+				client_socket.close()
+				sys.stdout.write(str(address)+" closed.\n")
+				qd.put(str(KEY))
+				flag = False
 		except:
 			sys.stdout.write(str(address)+" closed due to error.\n")
-			qc.get()
-			break
-		
+			qd.put(str(KEY))
+			flag = False
+		break
+
+	if flag:
+		sys.stdout.write(str(address)+" added to queue.\n")
+
 
 
 if __name__=='__main__':
